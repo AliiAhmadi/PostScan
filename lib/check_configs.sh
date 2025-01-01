aa2faaadefa8baee0ff29b30e7993d12a664d561
Lexur <107758775+AliiAhmadi@users.noreply.github.com>
2025-01-01 07:25:59 +0330

    Update check_configs.sh

--- lib/check_configs.sh
+++ lib/check_configs.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 
 
+# Function to check PostgreSQL security configurations
 check_security_configs(){
     echo -e "${YELLOW}[*] Checking PostgreSQL configuration...${RESET}"
-    CONFIG_FILE="/var/lib/pgsql/data/pg_hba.conf"
+    CONFIG_FILE="/var/lib/pgsql/data/pg_hba.conf"     # Path to PostgreSQL configuration file
 
 
+    # Check if the configuration file exists
     if [[ ! -f $CONFIG_FILE  ]]; then
         echo -e "${RED}[-] Configuration file not found!${RESET}"
-        return 1
+        return 1    # Exit the function with an error code
     fi
 
+    # Check if PostgreSQL is listening on all addresses (0.0.0.0)
     if grep -q "^listen_addresses = '0.0.0.0'" "$CONFIG_FILE"; then
         echo -e "${RED}[-] Warning: PostgreSQL is listening on all addresses!${RESET}"
     else
         echo -e "${GREEN}[+] Secure listening address configuration.${RESET}"
     fi
 
+    # Check if SSL is disabled in the configuration
     if grep -q "^ssl = off" "$CONFIG_FILE"; then
         echo -e "${RED}[-] Warning: SSL is disabled!${RESET}"
     else
