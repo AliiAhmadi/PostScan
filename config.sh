6b9dd0eba0446a151102bf58c42cf7c3871d725e
Lexur <107758775+AliiAhmadi@users.noreply.github.com>
2025-01-01 07:23:56 +0330

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-DB_USER="testuser"
-DB_HOST="localhost"
-DB_NAME="cve_test"
-DB_PORT="5432"
-DB_PASSWORD="1234"
+# Define database connection details
+DB_USER="testuser"      # Database username 
+DB_HOST="localhost"     # Database host (local machine)
+DB_NAME="cve_test"      # Database name
+DB_PORT="5432"          # Database port (default for PostgreSQL)
+DB_PASSWORD="1234"      # Database password
